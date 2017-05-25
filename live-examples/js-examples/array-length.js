a4381a9bc1ee0b9f2265467a72deaa8c1f7b8dd8
schalkneethling <sneethling@mozilla.com>
2017-05-25 13:45:51 +0200

    Adds Array.length js example

---
+++ live-examples/js-examples/array-length.js
@@ -0,0 +1,8 @@
+var cmInitContent = `// create an array
+var items = ['shoes', 'shirts', 'socks', 'sweaters'];
+
+// call the length property, and log the result
+console.log(items.length);`;
+
+var cmSelectLine = 1;
+var cmSelectChStart = 13;
