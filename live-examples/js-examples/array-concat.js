6fbb21b3aaffabb64e856c70dc0613e94be316fc
schalkneethling <sneethling@mozilla.com>
2017-05-25 14:22:32 +0200

    Adds Array.concat js example

---
+++ live-examples/js-examples/array-concat.js
@@ -0,0 +1,13 @@
+var cmInitContent = `// create two arrays
+var arr1 = ['a', 'b', 'c'];
+var arr2 = ['d', 'e', 'f'];
+
+/* call concat() on the first array passing
+   the second as an argument */
+var arr3 = arr1.concat(arr2);
+
+// log the result
+console.log(arr3);`;
+
+var cmSelectLine = 2;
+var cmSelectChStart = 25;
