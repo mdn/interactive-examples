ad9387cd83267f04509c5c65e6228b7cf120a679
schalkneethling <sneethling@mozilla.com>
2017-05-26 16:21:37 +0200

    Adds Array.join js example

---
+++ live-examples/js-examples/array-join.js
@@ -0,0 +1,11 @@
+var cmInitContent = `// create an array
+var elements = ['Fire', 'Wind', 'Rain'];
+
+// call join(), passing an optional separator
+var result = elements.join('-');
+
+// log the result
+console.log(result);`;
+
+var cmSelectLine = 4;
+var cmSelectChStart = 27;
