eaf29d0d26548380c92a65a42136a02e036085f0
schalkneethling <sneethling@mozilla.com>
2017-05-26 16:42:17 +0200

    Adds Array.pop js example

---
+++ live-examples/js-examples/array-pop.js
@@ -0,0 +1,11 @@
+var cmInitContent = `// create an array
+var brassicaOleracea = ['brocolli', 'cauliflower', 'cabbage', 'kale', 'tomato']
+
+// call pop(), to pop the last element
+brassicaOleracea.pop();
+
+// log the result
+console.log(brassicaOleracea);`;
+
+var cmSelectLine = 1;
+var cmSelectChStart = 24;
