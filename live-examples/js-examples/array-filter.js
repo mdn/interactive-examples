786f470abd7398650f2833afa015e24ffd7c99ad
schalkneethling <sneethling@mozilla.com>
2017-05-26 12:21:53 +0200

    Adds Array.filter js example

---
+++ live-examples/js-examples/array-filter.js
@@ -0,0 +1,13 @@
+var cmInitContent = `// create an array
+var words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+// Run filter against the array
+var result = words.filter(function(word) {
+    return word.length > 6;
+});
+
+// log the result
+console.log(result);`;
+
+var cmSelectLine = 1;
+var cmSelectChStart = 77;
