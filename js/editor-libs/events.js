c242f476772f52befe6e10f7fefaf6734ffe1356
schalkneethling <sneethling@mozilla.com>
2017-07-12 14:59:10 +0200

    Fix issue #101, hide reset button until first keyup event

--- js/editor-libs/events.js
+++ js/editor-libs/events.js
@@ -1,13 +1,27 @@
 (function() {
     'use strict';
 
-    var liveEditor = document.getElementById('live');
+    var liveEditorContainer = document.getElementById('live');
+    var liveEditor = document.getElementById('editor');
 
-    liveEditor.addEventListener('click', function(event) {
+    /**
+     * Enable and show the reset button on first keyup inside the editor
+     */
+    function showReset() {
+        var resetButton = document.getElementById('reset');
+        resetButton.classList.remove('hidden');
+        resetButton.setAttribute('aria-hidden', false);
+        // we can now safely remove the event listener
+        liveEditor.removeEventListener('keyup', showReset);
+    }
+
+    liveEditorContainer.addEventListener('click', function(event) {
         switch (event.target.id) {
         case 'execute':
             mceAnalytics.trackRunClicks();
             break;
         }
     });
+
+    liveEditor.addEventListener('keyup', showReset);
 })();
