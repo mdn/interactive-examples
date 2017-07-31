50ca121818847120a22eae579bc2a19d00fc087a
Dave Parfitt <diparfitt@gmail.com>
2017-07-31 10:14:09 -0400

    wip

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/bash -x
 cd "$(dirname ${BASH_SOURCE[0]})"/..
 echo "Starting build"
-docker run -v "$PWD":/mdn -w /mdn node:latest npm install && npm run build
+docker run -v "$PWD":/mdn -w /mdn node:latest bash -c "/usr/local/bin/npm install && /usr/local/bin/npm run build"
 echo "Build finished"
