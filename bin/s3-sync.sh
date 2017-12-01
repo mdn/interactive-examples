00df98e57c79e40fbb1e6fc03c37a1b75c022559
Josh Mize <jgmize@gmail.com>
2017-12-01 14:21:43 -0600

    Fix s3 sync paths

--- bin/s3-sync.sh
+++ bin/s3-sync.sh
@@ -11,12 +11,12 @@ ARGS="--acl public-read --delete --profile mdninteractive"
 cd docs
 
 for path in "pages live-examples"; do
-    aws s3 sync $path s3://$1 ${SHORT_CACHE} ${ARGS}
+    aws s3 sync $path s3://$1/$path ${SHORT_CACHE} ${ARGS}
 done
 
 for path in "css js"; do
-    aws s3 sync $path s3://$1 --exclude '*' --include 'editor-*' ${SHORT_CACHE} ${ARGS}
-    aws s3 sync $path s3://$1 --exclude 'editor-*' ${LONG_CACHE} ${ARGS}
+    aws s3 sync $path s3://$1/$path --exclude '*' --include 'editor-*' ${SHORT_CACHE} ${ARGS}
+    aws s3 sync $path s3://$1/$path --exclude 'editor-*' ${LONG_CACHE} ${ARGS}
 done
 
-aws s3 sync media s3://$1 ${LONG_CACHE} ${ARGS}
+aws s3 sync media s3://$1/media ${LONG_CACHE} ${ARGS}
