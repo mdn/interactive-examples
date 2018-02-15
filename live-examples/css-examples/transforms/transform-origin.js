30e5bce42e4c166ea083e7fe1c63e417e16d1085
mfluehr <radick334@yahoo.com>
2018-02-15 09:33:43 -0500

    Fixes

--- live-examples/css-examples/transforms/transform-origin.js
+++ live-examples/css-examples/transforms/transform-origin.js
@@ -1,35 +1,36 @@
-window.addEventListener("load", function () {
-    function update (mutations) {
-        var selected = document.querySelector(".selected code");
+'use strict';
 
-        // Restart the animation
-        // https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Tips
-        el.className = "";
-        window.requestAnimationFrame(function (time) {
-            window.requestAnimationFrame(function (time) {
+window.addEventListener('load', function() {
+    function update (mutations) {
+        var selected = document.querySelector('.selected code');
+
+        /* Restart the animation
+           https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Animations/Tips */
+        el.className = '';
+        window.requestAnimationFrame(function(time) {
+            window.requestAnimationFrame(function(time) {
                 el.className = selected.dataset.animation;
             });
         });
 
-
         var transformOrigin = getComputedStyle(el).transformOrigin;
         var pos = transformOrigin.split(/\s+/);
-        crosshair.style.left = "calc(" + pos[0] + " - 12px)";
-        crosshair.style.top = "calc(" + pos[1] + " - 12px)";
+        crosshair.style.left = 'calc(' + pos[0] + ' - 12px)';
+        crosshair.style.top = 'calc(' + pos[1] + ' - 12px)';
     }
 
-    var crosshair = document.getElementById("crosshair");
-    var el = document.getElementById("example-element");
+    var crosshair = document.getElementById('crosshair');
+    var el = document.getElementById('example-element');
 
-    var observer = new MutationObserver(function (mutations) {
+    var observer = new MutationObserver(function(mutations) {
         update(mutations);
     });
 
     observer.observe(el, {
         attributes: true,
-        attributeFilter: ["style"]
+        attributeFilter: ['style']
     });
 
     update();
-    crosshair.style.opacity = "1";
+    crosshair.style.opacity = '1';
 });
