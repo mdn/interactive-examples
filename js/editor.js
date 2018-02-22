d96635503641ff9a8e65bc897eea446a94bc5c41
wbamberg <wbamberg@mozilla.com>
2018-02-22 12:07:11 -0800

    Revert "Adds next iteration of the HTML interactive editor (#576)"
    This reverts commit 432e90b7c71b3a57bb5222128ee7f4f8bba1a180.

--- js/editor.js
+++ js/editor.js
@@ -1,36 +1,50 @@
 (function() {
     'use strict';
 
-    var mceUtils = require('./editor-libs/mce-utils');
-    var tabby = require('./editor-libs/tabby');
-
-    var cssEditor = document.getElementById('css-editor');
     var header = document.querySelector('.output-header');
     var htmlEditor = document.getElementById('html-editor');
-    var runButton = document.getElementById('run');
-    var staticCSSCode = cssEditor.querySelector('pre');
-    var staticHTMLCode = htmlEditor.querySelector('pre');
+    var staticExample = htmlEditor.querySelector('pre');
     var output = document.getElementById('output');
+
+    var editors = {
+        html: {
+            editor: undefined,
+            code: htmlEditor,
+            config: {
+                lineNumbers: true,
+                mode: 'htmlmixed',
+                value: staticExample.querySelector('code').textContent
+            }
+        }
+    };
+
+    /**
+     * Initialise the specified editor if not already initialised
+     * @param {String} editorType - The editor to initialise
+     */
+    function initEditor(editorType) {
+        if (editors[editorType].editor !== undefined) {
+            return;
+        }
+
+        // eslint-disable-next-line new-cap
+        editors[editorType].editor = CodeMirror(
+            editors[editorType].code,
+            editors[editorType].config
+        );
+    }
 
     header.addEventListener('click', function(event) {
         if (event.target.classList.contains('reset')) {
             window.location.reload();
         } else if (event.target.classList.contains('run')) {
-            output.innerHTML = mceUtils.getOutput();
+            output.innerHTML = editors.html.editor.getValue();
         }
     });
 
-    // hide the static example when JS enabled
-    staticHTMLCode.classList.add('hidden');
-    // hide the static CSS example
-    staticCSSCode.classList.add('hidden');
+    // add class live to body when JS enabled
+    staticExample.classList.add('hidden');
     // show the header
     header.classList.remove('hidden');
-
-    // initialise the editors
-    tabby.initEditor(['html', 'css']);
-    tabby.registerEventListeners();
-
-    // apply the current source on load
-    runButton.click();
+    initEditor('html');
 })();
