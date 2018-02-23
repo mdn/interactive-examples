2f22faceb1a9e74e8f3f4b6db6dba83ef77b3fd6
Daniel D. Beck <daniel@ddbeck.com>
2018-02-23 14:39:02 +0000

    Redo animation-fill-mode to show backwards and both values

--- live-examples/css-examples/animation/animation-fill-mode.js
+++ live-examples/css-examples/animation/animation-fill-mode.js
@@ -1,7 +1,11 @@
 'use strict';
 
 window.addEventListener('load', function() {
+    var el = document.getElementById('example-element');
+    var status = document.getElementById('playstatus');
+
     function update() {
+        status.textContent = 'delaying';
         el.className = '';
         window.requestAnimationFrame(function() {
             window.requestAnimationFrame(function() {
@@ -10,7 +14,15 @@ window.addEventListener('load', function() {
         });
     }
 
-    var el = document.getElementById('example-element');
+    el.addEventListener('animationstart', function(){
+        console.log('animationstart event');
+        status.textContent = 'playing';
+    });
+
+    el.addEventListener('animationend', function(){
+        console.log('animationend event');
+        status.textContent = 'finished';
+    });
 
     var observer = new MutationObserver(function(mutations) {
         update(mutations);
