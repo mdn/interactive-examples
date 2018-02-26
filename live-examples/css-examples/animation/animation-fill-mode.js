c6d1d317275091ade20e700507366b86b3161e9c
Daniel D. Beck <daniel@ddbeck.com>
2018-02-26 18:31:14 +0000

    Add example for animation-fill-mode CSS property (#602)
    * Add example for `animation-fill-mode` CSS property
    
    * Redo animation-fill-mode to show backwards and both values
    
    * Shorten delay for animation-fill-mode example
    
    * Fix jerky start to animation-fill-mode example
    
    * Increase text contrast in animation-fill-mode example

---
+++ live-examples/css-examples/animation/animation-fill-mode.js
@@ -0,0 +1,37 @@
+'use strict';
+
+window.addEventListener('load', function() {
+    var el = document.getElementById('example-element');
+    var status = document.getElementById('playstatus');
+
+    function update() {
+        status.textContent = 'delaying';
+        el.className = '';
+        window.requestAnimationFrame(function() {
+            window.requestAnimationFrame(function() {
+                el.className = 'animating';
+            });
+        });
+    }
+
+    el.addEventListener('animationstart', function(){
+        console.log('animationstart event');
+        status.textContent = 'playing';
+    });
+
+    el.addEventListener('animationend', function(){
+        console.log('animationend event');
+        status.textContent = 'finished';
+    });
+
+    var observer = new MutationObserver(function(mutations) {
+        update(mutations);
+    });
+
+    observer.observe(el, {
+        attributes: true,
+        attributeFilter: ['style']
+    });
+
+    update();
+});
