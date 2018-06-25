fb874571d616415e72ad575d490da8f153dc2578
Schalk Neethling <sneethling@mozilla.com>
2018-06-25 14:33:54 +0200

    Post load events to Kuma to mark, measure, and beacon

--- js/editor-libs/perf.js
+++ js/editor-libs/perf.js
@@ -2,38 +2,39 @@
     'use strict';
 
     /**
-     * If `performance.mark` and `performance.measure` is supported,
-     * it set’s marks at various stages during the document loading
-     * process, and exposes the following measurements:
-     * 1. Time to interactive
-     * 2. Time to DOM complete
+     * Posts a name to set as a mark to Kuma for
+     * processing and beaconing to GA
+     * @param {Object} perf - The performance object sent to Kuma
      */
-    if (performance.mark && performance.measure) {
-        performance.mark('interactive-editor-loading');
-
-        document.addEventListener('readystatechange', function(event) {
-            switch (event.target.readyState) {
-            case 'interactive':
-                performance.mark('interactive-editor-interactive');
-                if (performance.measure !== undefined) {
-                    performance.measure(
-                        'ie-time-to-interactive',
-                        'interactive-editor-loading',
-                        'interactive-editor-interactive'
-                    );
-                }
-                break;
-            case 'complete':
-                performance.mark('interactive-editor-complete');
-                if (performance.measure !== undefined) {
-                    performance.measure(
-                        'ie-time-to-complete',
-                        'interactive-editor-loading',
-                        'interactive-editor-complete'
-                    );
-                }
-                break;
-            }
-        });
+    function postToKuma(perf) {
+        window.parent.postMessage(perf, 'https://developer.mozilla.org');
     }
+
+    /**
+     * Posts marks to set on the Kuma side, based on certain
+     * events during document loading. These will then be made
+     * available in performance tools, and beaconed to GA
+     */
+    postToKuma({ markName: 'interactive-editor-loading' });
+
+    document.addEventListener('readystatechange', function(event) {
+        switch (event.target.readyState) {
+        case 'interactive':
+            postToKuma({
+                markName: 'interactive-editor-interactive',
+                measureName: 'ie-time-to-interactive',
+                startMark: 'interactive-editor-loading',
+                endMark: 'interactive-editor-interactive'
+            });
+            break;
+        case 'complete':
+            postToKuma({
+                markName: 'interactive-editor-complete',
+                measureName: 'ie-time-to-complete',
+                startMark: 'interactive-editor-loading',
+                endMark: 'interactive-editor-complete'
+            });
+            break;
+        }
+    });
 })();
