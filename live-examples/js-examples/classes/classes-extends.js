11bc6177308d694ead67297df3eb25dbdbc213a7
Will Bamberg <wbamberg@mozilla.com>
2020-01-21 10:58:56 -0800

    Trim blank line so we fit in standard editor

--- live-examples/js-examples/classes/classes-extends.js
+++ live-examples/js-examples/classes/classes-extends.js
@@ -3,7 +3,6 @@ class formatDate extends Date {
   getFormattedDate() {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
     return `${this.getDate()}-${months[this.getMonth()]}-${this.getFullYear()}`;
   }
 
