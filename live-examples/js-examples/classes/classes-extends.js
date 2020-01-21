a1f0fdfcf9f0c6bbe24732123dc864e8e6e43395
totalamd <totalamd@users.noreply.github.com>
2020-01-21 21:02:52 +0200

    no need constructor at all (#1526)
    * no need constructor at all
    
    * Use JS file for this example
    
    * Trim blank line so we fit in standard editor
    
    Co-authored-by: wbamberg <wbamberg@mozilla.com>

--- live-examples/js-examples/classes/classes-extends.js
+++ live-examples/js-examples/classes/classes-extends.js
@@ -1,14 +1,11 @@
 class formatDate extends Date {
-  constructor(dateStr) {
-    super(dateStr);
-  }
 
   getFormattedDate() {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
     return `${this.getDate()}-${months[this.getMonth()]}-${this.getFullYear()}`;
   }
+
 }
 
 console.log(new formatDate('August 19, 1975 23:15:30').getFormattedDate());
