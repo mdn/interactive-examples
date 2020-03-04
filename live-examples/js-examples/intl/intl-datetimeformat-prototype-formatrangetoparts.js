004a48e7eeedd9c6109616b473c141fe5d909b3d
Will Bamberg <wbamberg@mozilla.com>
2020-03-04 11:35:05 -0800

    Fix missing first line of Intl.DateTimeFormat.prototype.formatRangeToParts() example

--- live-examples/js-examples/intl/intl-datetimeformat-prototype-formatrangetoparts.js
+++ live-examples/js-examples/intl/intl-datetimeformat-prototype-formatrangetoparts.js
@@ -1,4 +1,4 @@
- 'Wed, 10 Jan 2007 10:00:00 GMT'
+const startDate = new Date(Date.UTC(2007, 0, 10, 10, 0, 0)); // > 'Wed, 10 Jan 2007 10:00:00 GMT'
 const endDate = new Date(Date.UTC(2007, 0, 10, 11, 0, 0));   // > 'Wed, 10 Jan 2007 11:00:00 GMT'
 
 const dateTimeFormat = new Intl.DateTimeFormat("en", {
