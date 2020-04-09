367e2738ebc3dd1360e9209a085ec06243950406
Abdoulaye Siby <maestro@abdoulaye.com>
2020-04-08 22:05:06 -0400

    Updated the interactive example for Map.prototype.get().

--- live-examples/js-examples/map/map-prototype-get.js
+++ live-examples/js-examples/map/map-prototype-get.js
@@ -1,8 +1,19 @@
 const map1 = new Map();
 map1.set('bar', 'foo');
+map1.set('myBarObject', { name: 'foo' });
 
 console.log(map1.get('bar'));
 // expected output: "foo"
 
 console.log(map1.get('baz'));
 // expected output: undefined
+
+let myObject = map1.get('myBarObject');
+
+console.log(map1.get('myBarObject'));
+// expected output: { name: 'foo' }
+
+myObject.score = 12;
+
+console.log(map1.get('myBarObject'));
+// expected output: { name: 'foo', score: 12 }
