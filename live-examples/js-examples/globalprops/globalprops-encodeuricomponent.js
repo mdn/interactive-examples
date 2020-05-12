877032a3195abe70fb83891c6ee800389fd05c15
LariSinisalo <65231729+LariSinisalo@users.noreply.github.com>
2020-05-12 15:41:56 +0300

    Update globalprops-encodeuricomponent.js
    Do not escape the url parts required for parameters to parse correctly if appended to an url.
    
    This makes the provided example be closer to what users typically would want to do.

--- live-examples/js-examples/globalprops/globalprops-encodeuricomponent.js
+++ live-examples/js-examples/globalprops/globalprops-encodeuricomponent.js
@@ -1,6 +1,6 @@
 // encodes characters such as ?,=,/,&,:
-console.log(encodeURIComponent('?x=шеллы'));
-// expected output: "%3Fx%3D%D1%88%D0%B5%D0%BB%D0%BB%D1%8B"
+console.log('?x='+encodeURIComponent('test?'));
+// expected output: "?x=test%3F"
 
-console.log(encodeURIComponent('?x=test'));
-// expected output: "%3Fx%3Dtest"
+console.log('?x='+encodeURIComponent('шеллы'));
+// expected output: "?x=%D1%88%D0%B5%D0%BB%D0%BB%D1%8B"
