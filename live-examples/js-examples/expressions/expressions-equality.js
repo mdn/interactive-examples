6cf31a8ed977841d0c1d4c86ec89a25c313e1813
Will Bamberg <wbamberg@mozilla.com>
2020-06-09 17:17:57 -0700

    Add interactive examples for comparison operators

---
+++ live-examples/js-examples/expressions/expressions-equality.js
@@ -0,0 +1,11 @@
+console.log(1 == 1);
+// expected output: true
+
+console.log('hello' == 'hello');
+// expected output: true
+
+console.log('1' ==  1);
+// expected output: true
+
+console.log(0 == false);
+// expected output: true
