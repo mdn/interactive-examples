143a9daa04dd00b321e2f4fcf26edb2fa6ef579b
Florian Scholz <fscholz@mozilla.com>
2020-07-15 01:06:07 +0200

    Add Promise.any (#1616)
    * Add Promise.any
    
    * nits

---
+++ live-examples/js-examples/promise/promise-any.js
@@ -0,0 +1,9 @@
+const promise1 = Promise.reject(0);
+const promise2 = new Promise((resolve) => setTimeout(resolve, 100, 'quick'));
+const promise3 = new Promise((resolve) => setTimeout(resolve, 500, 'slow'));
+
+const promises = [promise1, promise2, promise3];
+
+Promise.any(promises).then((value) => console.log(value));
+
+// expected output: "quick"
