91c9b1354548bbd1699c3e996cf443cc5eb3f430
Micah <tigleym@gmail.com>
2020-08-09 13:52:57 -0400

    Add an example for Array.flat() (#1655)
    * Add an example for Array.flat()
    
    * Update comments to be more descriptive of what .flat() is doing.
    
    * Remove comments for examples

---
+++ live-examples/js-examples/array/array-flat.js
@@ -0,0 +1,9 @@
+const arr1 = [0, 1, 2, [3, 4]];
+
+console.log(arr1.flat());
+// expected output: [0, 1, 2, 3, 4]
+
+const arr2 = [0, 1, 2, [[[3, 4]]]];
+
+console.log(arr2.flat(2));
+// expected output: [0, 1, 2, [3, 4]]
