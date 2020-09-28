f9b3e63cc31297770951b613f27a57fb0ec51e16
Velu S Gautam <i@velusgautam.com>
2020-09-28 17:54:27 +0530

    Update var to const in css-examples js files Removed unused variables

--- live-examples/css-examples/animation/play-pause.js
+++ live-examples/css-examples/animation/play-pause.js
@@ -1,15 +1,14 @@
 'use strict';
 
-window.addEventListener('load', function() {
-    var el = document.getElementById('example-element');
-    var button = document.getElementById('play-pause');
+window.addEventListener('load', function () {
+    const el = document.getElementById('example-element');
+    const button = document.getElementById('play-pause');
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         if (el.classList.contains('running')) {
             el.classList.remove('running');
             button.textContent = 'Play';
-        }
-        else {
+        } else {
             el.classList.add('running');
             button.textContent = 'Pause';
         }
