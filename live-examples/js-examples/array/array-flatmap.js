48bdb7575fd9e79840e0f3812cc5df5886ac9a13
Talita Oliveira <litaa.oliveira@gmail.com>
2020-10-03 22:26:08 -0300

    Add example for Array.flatMap()

---
+++ live-examples/js-examples/array/array-flatmap.js
@@ -0,0 +1,29 @@
+const humans = [
+  {
+    name: 'Human 1',
+    phones: [
+      { type: 'cellphone', number: '9.9999-9999' },
+      { type: 'landline', number: '3444-4444' },
+    ],
+  },
+  {
+    nome: 'Human 2',
+    phones: [
+      { type: 'cellphone', number: '9.7777-7777' },
+      { type: 'cellphone', number: '9.6666-6666' },
+      { type: 'landline', number: '3466-6666' },
+    ],
+  },
+];
+
+const flatten = humans.flatMap((human) => human.phones);
+
+console.log(flatten);
+/* expected output: [
+  { type: 'cellphone', number: '9.9999-9999' },
+  { type: 'landline', number: '3444-4444' },
+  { type: 'cellphone', number: '9.7777-7777' },
+  { type: 'cellphone', number: '9.6666-6666' },
+  { type: 'landline', number: '3466-6666' }
+]
+*/
