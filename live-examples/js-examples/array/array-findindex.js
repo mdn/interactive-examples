756d0ba17375a2521a1e32d138cd93ca726062ed
Benjamin Gruenbaum <inglor@gmail.com>
2020-10-21 11:57:31 +0300

    Update array-findindex.js
    After feedback I got regarding the current example - show an anonymous function first.

--- live-examples/js-examples/array/array-findindex.js
+++ live-examples/js-examples/array/array-findindex.js
@@ -1,6 +1,11 @@
 const array1 = [5, 12, 8, 130, 44];
 
-const isLargeNumber = (element) => element > 13;
+console.log(array1.findIndex((element) => element > 13));
+// expected output: 3
+
+// or with a named function
+
+const isLargeNumber = (element) => element > 13);
 
 console.log(array1.findIndex(isLargeNumber));
 // expected output: 3
