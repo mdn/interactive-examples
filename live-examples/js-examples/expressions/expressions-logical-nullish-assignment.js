f03d7860534eee3e020096c57dd6350587e4482d
David Gilbertson <gilbertson.david@gmail.com>
2020-10-24 07:39:36 +1100

    Update expressions-logical-nullish-assignment.js

--- live-examples/js-examples/expressions/expressions-logical-nullish-assignment.js
+++ live-examples/js-examples/expressions/expressions-logical-nullish-assignment.js
@@ -1,8 +1,8 @@
-const a = { duration: 50 };
+const a = { duration: 0 };
 
 a.duration ??= 10;
 console.log(a.duration);
-// expected output: 50
+// expected output: 0
 
 a.speed ??= 25;
 console.log(a.speed);
