aebfa361659c43ba6ecd6c0d551463cebdaedff9
Talita Oliveira <litaa.oliveira@gmail.com>
2020-10-23 17:47:48 -0300

    Change example to the given suggestion

--- live-examples/js-examples/array/array-flatmap.js
+++ live-examples/js-examples/array/array-flatmap.js
@@ -1,29 +1,5 @@
-const humans = [
-  {
-    name: 'Human 1',
-    phones: [
-      { type: 'cellphone', number: '9.9999-9999' },
-      { type: 'landline', number: '3444-4444' },
-    ],
-  },
-  {
-    name: 'Human 2',
-    phones: [
-      { type: 'cellphone', number: '9.7777-7777' },
-      { type: 'cellphone', number: '9.6666-6666' },
-      { type: 'landline', number: '3466-6666' },
-    ],
-  },
-];
+const nums = [ 1, 2, [3], [4], [] ];
+const flattened = nums.flatMap(num => num);
 
-const flatten = humans.flatMap((human) => human.phones);
-
-console.log(flatten);
-/* expected output: [
-  { type: 'cellphone', number: '9.9999-9999' },
-  { type: 'landline', number: '3444-4444' },
-  { type: 'cellphone', number: '9.7777-7777' },
-  { type: 'cellphone', number: '9.6666-6666' },
-  { type: 'landline', number: '3466-6666' }
-]
-*/
+console.log(flattened);
+// [ 1, 2, 3, 4 ]
