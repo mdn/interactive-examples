7b3f72de9633a9592eb34bb617d2f1f9d94ed36c
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2020-12-08 18:44:50 -0500

    Shortened Intl.DateTimeFormat.formatToParts example

--- live-examples/js-examples/intl/intl-datetimeformat-prototype-formattoparts.js
+++ live-examples/js-examples/intl/intl-datetimeformat-prototype-formattoparts.js
@@ -3,24 +3,7 @@ const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric
 const dateTimeFormat = new Intl.DateTimeFormat('en-US', options);
 
 const parts = dateTimeFormat.formatToParts(date);
+const partValues = parts.map(p => p.value);
 
-console.log(parts[0].value);
-// expected output: "Friday"
-
-console.log(parts[1].value);
-// expected output: ", "
-
-console.log(parts[2].value);
-// expected output: "June"
-
-console.log(parts[3].value);
-// expected output: " "
-
-console.log(parts[4].value);
-// expected output: "1"
-
-console.log(parts[5].value);
-// expected output: ", "
-
-console.log(parts[6].value);
-// expected output: "2012"
+console.log(partValues);
+// expected output: "["Friday", ", ", "June", " ", "1", ", ", "2012"]"
