3364d11f55cedfead24e1d3593a480960ba45d4f
alexanderhess <Hess.alexander1@web.de>
2020-12-16 17:55:16 +0100

    fixed issues using the linter

--- live-examples/js-examples/array/array-flatMap.js
+++ live-examples/js-examples/array/array-flatMap.js
@@ -1,9 +1,9 @@
-const arr1 = [1, 2, 3, 4];
+const array1 = [1, 2, 3, 4];
 
-const map1 = arr1.map(x => [x * 2]); 
-console.log(map1);
+const array2 = array1.map(x => [x * 2]);
+console.log(array2);
 // expected output: Array [[2], [4], [6], [8]]
 
-const flatmap1 = arr1.flatMap(x => [x * 2]);
-console.log(flatmap1);
+const array3 = array1.flatMap(x => [x * 2]);
+console.log(array3);
 // expected output: Array [2, 4, 6, 8]
