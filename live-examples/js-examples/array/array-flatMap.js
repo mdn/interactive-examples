1367099bab4a9966722e9c3e3a4e8786f1173297
alexanderhess <Hess.alexander1@web.de>
2020-12-16 17:32:49 +0100

    Better Quality

--- live-examples/js-examples/array/array-flatMap.js
+++ live-examples/js-examples/array/array-flatMap.js
@@ -1,7 +1,9 @@
-let arr1 = [1, 2, 3, 4];
+const arr1 = [1, 2, 3, 4];
 
-arr1.map(x => [x * 2]); 
-// [[2], [4], [6], [8]]
+const map1 = arr1.map(x => [x * 2]); 
+console.log(map1);
+// expected output: Array [[2], [4], [6], [8]]
 
-arr1.flatMap(x => [x * 2]);
-// [2, 4, 6, 8]
+const flatmap1 = arr1.flatMap(x => [x * 2]);
+console.log(flatmap1);
+// expected output: Array [2, 4, 6, 8]
