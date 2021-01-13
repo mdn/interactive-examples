482232936979ff6cda9e41805d352d2bc70c983b
Chris Mills <cmills@mozilla.com>
2021-01-13 13:12:59 +0000

    Update array-at.js

--- live-examples/js-examples/array/array-at.js
+++ live-examples/js-examples/array/array-at.js
@@ -2,10 +2,10 @@ const array1 = [5, 12, 8, 130, 44];
 
 let index = 2;
 
-console.log(`Using a positive index the item returned is ${array1.at(index)}`);
-// expected output: "Using a positive index the item returned is 12"
+console.log(`Using an index of ${index} the item returned is ${array1.at(index)}`);
+// expected output: "Using an index of 2 the item returned is 8"
 
 index = -2;
 
-console.log(`Using a negative index the item returned is ${array1.at(index)}`);
-// expected output: "Using a negative index the item returned is 130"
+console.log(`Using an index of ${index} item returned is ${array1.at(index)}`);
+// expected output: "Using an index of -2 item returned is 130"
