20361d6303f3ee6b7fc4a9524ac392d7c2bb303c
Chris Mills <cmills@mozilla.com>
2021-01-13 13:14:17 +0000

    Update typedarray-at.js

--- live-examples/js-examples/typedarray/typedarray-at.js
+++ live-examples/js-examples/typedarray/typedarray-at.js
@@ -2,10 +2,10 @@ const int8 = new Int8Array([0, 10, -10, 20, -30, 40, -50]);
 
 let index = 1;
 
-console.log(`Using a positive index the item returned is ${int8.at(index)}`);
-// expected output: "Using a positive index the item returned is 10"
+console.log(`Using an index of ${index} the item returned is ${int8.at(index)}`);
+// expected output: "Using an index of 1 the item returned is 10"
 
 index = -2;
 
-console.log(`Using a negative index the item returned is ${int8.at(index)}`);
-// expected output: "Using a negative index the item returned is 40"
+console.log(`Using an index of ${index} the item returned is ${int8.at(index)}`);
+// expected output: "Using an index of -2 the item returned is 40"
