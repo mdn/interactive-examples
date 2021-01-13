d087a4a0dceccab916d2566246ee30e3724408dc
Rumyra <rumyra@gmail.com>
2021-01-13 12:21:07 +0000

    updating

--- live-examples/js-examples/array/array-at.js
+++ live-examples/js-examples/array/array-at.js
@@ -1,6 +1,11 @@
 const array1 = [5, 12, 8, 130, 44];
 
-const item1 = array1.at(-3);
+let index = 2;
 
-console.log(item1);
-// expected output: 8
+console.log(`Using a positive index the item returned is ${array1.at(index)}`);
+// expected output: "Using a positive index the item returned is 12"
+
+index = -2;
+
+console.log(`Using a negative index the item returned is ${array1.at(index)}`);
+// expected output: "Using a negative index the item returned is 130"
--- live-examples/js-examples/typedarray/typedarray-at.js
+++ live-examples/js-examples/typedarray/typedarray-at.js
@@ -1,4 +1,11 @@
-const int8 = new Int8Array([10, 0, -10, 20, -30, 40, -50]);
+const int8 = new Int8Array([0, 10, -10, 20, -30, 40, -50]);
 
-console.log(int8.at(-2));
-// expected output: 40
+let index = 1;
+
+console.log(`Using a positive index the item returned is ${int8.at(index)}`);
+// expected output: "Using a positive index the item returned is 10"
+
+index = -2;
+
+console.log(`Using a negative index the item returned is ${int8.at(index)}`);
+// expected output: "Using a negative index the item returned is 40"
