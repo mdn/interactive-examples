c1bfc4068fb1d4e409201557c02a08b17bd21cca
hinell <al.neodim@gmail.com>
2021-03-18 13:14:16 +0300

    Clarify example. Closes #1622

--- live-examples/js-examples/object/object-prototype-isprototypeof.js
+++ live-examples/js-examples/object/object-prototype-isprototypeof.js
@@ -1,12 +1,10 @@
-function object1() {}
-function object2() {}
+function Foo() {}
+function Bar() {}
 
-object1.prototype = Object.create(object2.prototype);
+Bar.prototype = Object.create(Foo.prototype);
 
-const object3 = new object1();
+const bar = new Bar();
 
-console.log(object1.prototype.isPrototypeOf(object3));
-// expected output: true
+console.log(Foo.prototype.isPrototypeOf(bar));
 
-console.log(object2.prototype.isPrototypeOf(object3));
-// expected output: true
+console.log(Bar.prototype.isPrototypeOf(bar));
