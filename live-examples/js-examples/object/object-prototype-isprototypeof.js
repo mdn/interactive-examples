20c866d5d7fa1f116dba0ec23cbf139cb5085dae
hinell <al.neodim@gmail.com>
2021-05-12 11:27:32 +0300

    Update object-prototype-isprototypeof.js

--- live-examples/js-examples/object/object-prototype-isprototypeof.js
+++ live-examples/js-examples/object/object-prototype-isprototypeof.js
@@ -6,6 +6,6 @@ Bar.prototype = Object.create(Foo.prototype);
 const bar = new Bar();
 
 console.log(Foo.prototype.isPrototypeOf(bar));
-
+// expected output: true
 console.log(Bar.prototype.isPrototypeOf(bar));
 // expected output: true
