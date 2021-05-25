c4016427c940f0ff4f0089709af1d27e0c3114c5
Akash Negi <55234838+NegiAkash890@users.noreply.github.com>
2021-05-25 19:33:07 +0530

    Added missing semicolon

--- live-examples/js-examples/array/array-slice.js
+++ live-examples/js-examples/array/array-slice.js
@@ -9,7 +9,7 @@ console.log(animals.slice(2, 4));
 console.log(animals.slice(1, 5));
 // expected output: Array ["bison", "camel", "duck", "elephant"]
 
-console.log(animals.slice(-2)) 
+console.log(animals.slice(-2));
 // expected output: Array ["duck", "elephant"]
 
 console.log(animals.slice(2, -1));
