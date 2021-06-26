5fd3fa7522f7a4ddb2770ba148711f1bf1092f79
hamirmahal <hamirmahal@gmail.com>
2021-06-26 10:17:57 -0700

    show extra Map features
    This commit addresses
    https://github.com/mdn/interactive-examples/pull/1855/files/3746fa11fa3a86a6cdc0e8f6f324356e73d104e8#r658918424.

--- live-examples/js-examples/map/map.js
+++ live-examples/js-examples/map/map.js
@@ -11,3 +11,11 @@ map1.set('a', 97);
 
 console.log(map1.get('a'));
 // expected output: 97
+
+console.log(map1.size);
+// expected output: 3
+
+map1.delete('b');
+
+console.log(map1.size);
+// expected output: 2
