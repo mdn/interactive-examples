e5c95a98066b61891bd1491994b1e558accb7f5c
Hamir Mahal <hamirmahal@gmail.com>
2021-06-29 12:11:33 -0700

    add example showcasing basic map functionality (#1855)
    * add example showcasing basic map functionality
    
    * change variable capitalization to match convention
    
    This commit addresses
    https://github.com/mdn/interactive-examples/pull/1855/files/3746fa11fa3a86a6cdc0e8f6f324356e73d104e8#r658916750.
    
    * omit `result` variable to save space
    
    This commit addresses
    https://github.com/mdn/interactive-examples/pull/1855/files/3746fa11fa3a86a6cdc0e8f6f324356e73d104e8#r658917195.
    
    * show extra Map features
    
    This commit addresses
    https://github.com/mdn/interactive-examples/pull/1855/files/3746fa11fa3a86a6cdc0e8f6f324356e73d104e8#r658918424.

---
+++ live-examples/js-examples/map/map.js
@@ -0,0 +1,21 @@
+const map1 = new Map();
+
+map1.set('a', 1);
+map1.set('b', 2);
+map1.set('c', 3);
+
+console.log(map1.get('a'));
+// expected output: 1
+
+map1.set('a', 97);
+
+console.log(map1.get('a'));
+// expected output: 97
+
+console.log(map1.size);
+// expected output: 3
+
+map1.delete('b');
+
+console.log(map1.size);
+// expected output: 2
