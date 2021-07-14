e63d1b2ce80820f1b6a7577b7c5fd9444b684119
Ruth John <Rumyra@users.noreply.github.com>
2021-07-14 18:13:02 +0100

    Async generator examples (#1784)
    * expression
    
    * statement
    
    * fixing
    
    * tidying up

--- live-examples/js-examples/statement/statement-async-function-asterisk.js
+++ live-examples/js-examples/statement/statement-async-function-asterisk.js
@@ -1,19 +1,19 @@
-async function* randomNumbers() {
-  const url = 'https://www.random.org/decimal-fractions/?num=1&dec=10&col=1&format=plain&rnd=new';
-
-  console.log('Print randomly generated numbers until reset is clicked, or the random number exceeds 0.95');
-  while (true) {
-    const response = await fetch(url);
-    const text = await response.text();
-    yield Number(text);
-  }
+async function* asyncGenerator() {
+  yield await Promise.resolve(1);
+  yield await Promise.resolve(2);
+  yield await Promise.resolve(3);
 }
 
-async function printRandoms() {
-  for await (const number of randomNumbers()) {
-    console.log(number);
-    if (number > 0.95) { break; }
-  }
-}
+const asyncGen = asyncGenerator();
 
-printRandoms();
+asyncGen.next()
+  .then(res => console.log(res.value));
+// expected output: 1
+
+asyncGen.next()
+  .then(res => console.log(res.value));
+// expected output: 2
+
+asyncGen.next()
+  .then(res => console.log(res.value));
+// expected output: 3
