3aec08aad9d40300bb791984b0d6c62fa666498b
Hamish Willee <hamishwillee@gmail.com>
2021-07-29 09:04:13 +1000

    force update

--- live-examples/js-examples/object/object-hasown.js
+++ live-examples/js-examples/object/object-hasown.js
@@ -8,6 +8,7 @@ object1.prop_value_null = null;
 console.log(Object.hasOwn(object1, 'prop_value_null'));  // expected output: true
 object1.prop_value_undefined = undefined;
 console.log(Object.hasOwn(object1, 'prop_value_undefined'));  // expected output: true
+
 // Inherited and undeclared values return false
 console.log(Object.hasOwn(object1,'toString'));  // expected output: false (inherited)
 console.log(Object.hasOwn(object1,'prop_not_defined'));  // expected output: false (not defined)
