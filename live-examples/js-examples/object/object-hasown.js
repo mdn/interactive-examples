d57c6c34d177a9a0171da2bb290aa0f99f1d0e0a
Hamish Willee <hamishwillee@gmail.com>
2021-08-01 18:33:26 -0700

    Fix properties to camel case

--- live-examples/js-examples/object/object-hasown.js
+++ live-examples/js-examples/object/object-hasown.js
@@ -4,11 +4,10 @@ if (Object.hasOwn(object1, 'prop')) {
   console.log('\'prop\' is own property'); // expected output: 'prop' is own property
 }
 // Property values can be null or undefined
-object1.prop_value_null = null;
-console.log(Object.hasOwn(object1, 'prop_value_null'));  // expected output: true
-object1.prop_value_undefined = undefined;
-console.log(Object.hasOwn(object1, 'prop_value_undefined'));  // expected output: true
-
+object1.nullPropertyValue = null;
+console.log(Object.hasOwn(object1, 'nullPropertyValue'));  // expected output: true
+object1.undefinedPropertyValue = undefined;
+console.log(Object.hasOwn(object1, 'undefinedPropertyValue'));  // expected output: true
 // Inherited and undeclared values return false
 console.log(Object.hasOwn(object1, 'toString'));  // expected output: false (inherited)
-console.log(Object.hasOwn(object1, 'prop_not_defined'));  // expected output: false (not defined)
+console.log(Object.hasOwn(object1, 'undeclaredPropertyValue'));  // expected output: false (not declared)
