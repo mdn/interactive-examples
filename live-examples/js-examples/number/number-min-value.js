844996f5a42cb5b88081989f38f85abed4756c46
Himanshu <hgarg@acm.org>
2021-08-10 15:55:04 +0530

    Removes references to unrelated -Infinity

--- live-examples/js-examples/number/number-min-value.js
+++ live-examples/js-examples/number/number-min-value.js
@@ -1,12 +1,12 @@
 function multiply(x, y) {
-  if (x * y < Number.MIN_VALUE) {
-    return 'Process as -Infinity';
-  }
-  return (x * y);
+  if (x > 0 && y > 0 && x * y < Number.MIN_VALUE) {
+    console.log('Underflow');
+  } 
+  
+  return x * y;
 }
 
-console.log(multiply(5e-324, 1));
-// expected output: 5e-324
+multiply(5e-324, 2));
 
-console.log(multiply(-1.7976931348623157e+308, 2));
-// expected output: Process as -Infinity
+multiply(5e-324, 0.5);
+// expected output: Underflow
