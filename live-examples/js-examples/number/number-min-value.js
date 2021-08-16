2ac15ade0cca1696bfd89146ff635f19b0076aa5
Biki-das <72331432+Biki-das@users.noreply.github.com>
2021-08-17 00:31:38 +0530

    Update example about Number.MIN_VALUE(#1890)
    Fixes mdn/content#7975

--- live-examples/js-examples/number/number-min-value.js
+++ live-examples/js-examples/number/number-min-value.js
@@ -1,6 +1,6 @@
 function multiply(x, y) {
   if (x * y < Number.MIN_VALUE) {
-    return 'Process as -Infinity';
+    return 'Process as 0';
   }
   return (x * y);
 }
@@ -8,5 +8,5 @@ function multiply(x, y) {
 console.log(multiply(5e-324, 1));
 // expected output: 5e-324
 
-console.log(multiply(-1.7976931348623157e+308, 2));
-// expected output: Process as -Infinity
+console.log(multiply(5e-324, 2));
+// expected output: Process as 0
