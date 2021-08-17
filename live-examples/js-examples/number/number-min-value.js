5962d0ff43c753f1f5daf56fcd8017ca45144f07
Anil Seervi <anil13112000@gmail.com>
2021-08-17 09:26:36 +0530

    Update example to be unambiguous

--- live-examples/js-examples/number/number-min-value.js
+++ live-examples/js-examples/number/number-min-value.js
@@ -1,12 +1,12 @@
-function multiply(x, y) {
-  if (x * y < Number.MIN_VALUE) {
-    return 'Process as 0';
+function divide(x, y) {
+  if (x / y < Number.MIN_VALUE) {
+    return `Process as 0`;
   }
-  return (x * y);
+  return (x / y);
 }
 
-console.log(multiply(5e-324, 1));
+console.log(divide(5e-324, 1));
 // expected output: 5e-324
 
-console.log(multiply(5e-324, 2));
+console.log(divide(5e-324, 2));
 // expected output: Process as 0
