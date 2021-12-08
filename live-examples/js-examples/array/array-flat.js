1b08da10f339ff820d2b4091e67dae23aa10b161
Bodo Graumann <mail@bodograumann.de>
2021-12-08 13:08:08 +0100

    Fix nesting depth in Array.prototype.flat example
    Nesting is reduced one level by array.flat. I have removed one level of nesting from the input to fix the semantics and at the same time make it more readable, while still illustrating the point.

--- live-examples/js-examples/array/array-flat.js
+++ live-examples/js-examples/array/array-flat.js
@@ -3,7 +3,7 @@ const arr1 = [0, 1, 2, [3, 4]];
 console.log(arr1.flat());
 // Expected output: Array [0, 1, 2, 3, 4]
 
-const arr2 = [0, 1, 2, [[[3, 4]]]];
+const arr2 = [0, 1, 2, [[3, 4]]];
 
 console.log(arr2.flat(2));
 // Expected output: Array [0, 1, 2, Array [3, 4]]
