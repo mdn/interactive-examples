df1fcf3a930e44764a16b79516b3efe3ddb202c8
wbamberg <will@bootbonnet.ca>
2022-01-10 00:39:24 -0800

    Update hasOwn example for readability (#1988)

--- live-examples/js-examples/object/object-hasown.js
+++ live-examples/js-examples/object/object-hasown.js
@@ -1,13 +1,12 @@
-const object1 = {};
-object1.prop = 'exists';
-if (Object.hasOwn(object1, 'prop')) {
-  console.log('\'prop\' is own property'); // expected output: 'prop' is own property
-}
-// Property values can be null or undefined
-object1.nullPropertyValue = null;
-console.log(Object.hasOwn(object1, 'nullPropertyValue'));  // expected output: true
-object1.undefinedPropertyValue = undefined;
-console.log(Object.hasOwn(object1, 'undefinedPropertyValue'));  // expected output: true
-// Inherited and undeclared values return false
-console.log(Object.hasOwn(object1, 'toString'));  // expected output: false (inherited)
-console.log(Object.hasOwn(object1, 'undeclaredPropertyValue'));  // expected output: false (not declared)
+const object1 = {
+  prop: 'exists'
+};
+
+console.log(Object.hasOwn(object1, 'prop'));
+// expected output: true
+
+console.log(Object.hasOwn(object1, 'toString'));
+// expected output: false
+
+console.log(Object.hasOwn(object1, 'undeclaredPropertyValue'));
+// expected output: false
