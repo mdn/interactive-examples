3f4df3a1c47c1204fa3031a51351ed329d3f6a1a
Jen Weber <j@jenweber.me>
2022-01-20 21:44:22 -0500

    Styleguide adjustments

--- live-examples/js-examples/array/array-reduce.js
+++ live-examples/js-examples/array/array-reduce.js
@@ -1,17 +1,19 @@
-const myArray = [1, 2, 3, 4];
+const array1 = [1, 2, 3, 4];
 
 // 1 + 2 + 3 + 4
-const result = myArray.reduce(
+const result = array1.reduce(
   (previousValue, currentValue) => previousValue + currentValue
 );
+
 console.log(result);
 // expected output: 10
 
 // 5 + 1 + 2 + 3 + 4
 const initialValue = 5;
-const resultWithInitial = myArray.reduce(
+const resultWithInitial = array1.reduce(
   (previousValue, currentValue) => previousValue + currentValue,
   initialValue
 );
+
 console.log(resultWithInitial);
 // expected output: 15
