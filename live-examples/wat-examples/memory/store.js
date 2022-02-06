fd5495fd1d9bc4a84123edae154bff72fbaacee1
Mendy Berger <12537668+MendyBerger@users.noreply.github.com>
2022-02-06 16:44:22 -0500

    Added wasm memory operators

---
+++ live-examples/wat-examples/memory/store.js
@@ -0,0 +1,14 @@
+var url = "{%wasm-url%}";
+WebAssembly.instantiateStreaming(fetch(url)).then(
+  (result) => {
+    const store_in_mem = result.instance.exports.store_in_mem;
+    const memory = result.instance.exports.memory;
+
+    store_in_mem(100);
+
+    var dataView = new DataView(memory.buffer);
+    var first_number_in_mem = dataView.getUint32(0, true);
+
+    console.log(first_number_in_mem);
+  }
+);
