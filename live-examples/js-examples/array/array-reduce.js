4aecea85908f5e87cf1ad92f9193701df1d8f360
Jen Weber <weberj10@gmail.com>
2022-02-10 22:11:15 -0500

    Show just one example

--- live-examples/js-examples/array/array-reduce.js
+++ live-examples/js-examples/array/array-reduce.js
@@ -1,19 +1,11 @@
 const array1 = [1, 2, 3, 4];
 
-// 1 + 2 + 3 + 4
-const sum = array1.reduce(
-  (previousValue, currentValue) => previousValue + currentValue
-);
-
-console.log(sum);
-// expected output: 10
-
 // 5 + 1 + 2 + 3 + 4
-const initialValue = 5;
+const initialValue = 0;
 const sumWithInitial = array1.reduce(
   (previousValue, currentValue) => previousValue + currentValue,
   initialValue
 );
 
 console.log(sumWithInitial);
-// expected output: 15
+// expected output: 10
