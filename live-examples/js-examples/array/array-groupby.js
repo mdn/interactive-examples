fdf667c56360b48cc03872b714c7e1e89b870a99
Hamish Willee <hamishwillee@gmail.com>
2022-02-21 10:16:59 +1100

    Fix lint errors

--- live-examples/js-examples/array/array-groupby.js
+++ live-examples/js-examples/array/array-groupby.js
@@ -1,9 +1,9 @@
 const inventory = [
-  {name: 'asparagus', type: 'vegetables', quantity: 5},
-  {name: 'bananas',  type: 'fruit', quantity: 0},
-  {name: 'goat', type: 'meat', quantity: 23},
-  {name: 'cherries', type: 'fruit', quantity: 5},
-  {name: 'fish', type: 'meat', quantity: 22}
+  { name: 'asparagus', type: 'vegetables', quantity: 5 },
+  { name: 'bananas',  type: 'fruit', quantity: 0 },
+  { name: 'goat', type: 'meat', quantity: 23 },
+  { name: 'cherries', type: 'fruit', quantity: 5 },
+  { name: 'fish', type: 'meat', quantity: 22 }
 ];
 
 const result = inventory.groupBy( ({ type }) => type );
