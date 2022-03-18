54dd8b71af25087f9d0e02dc26dfe46d70443128
SphinxKnight <SphinxKnight@users.noreply.github.com>
2022-03-18 23:09:44 +0100

    Initial set of live examples for Intl.Segmenter et al. (#1987)
    * Add an initial set of live examples for Intl.Segmenter et al.
    
    * shorten examples

---
+++ live-examples/js-examples/intl/intl-segmenter.js
@@ -0,0 +1,10 @@
+const segmenterFr = new Intl.Segmenter('fr', { granularity: 'word' });
+const string1 = 'Que ma joie demeure';
+
+const iterator1 = segmenterFr.segment(string1)[Symbol.iterator]();
+
+console.log(iterator1.next().value.segment);
+// expected output: 'Que'
+
+console.log(iterator1.next().value.segment);
+// expected output: ' '
