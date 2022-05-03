7bd1f593b4e9fb9845973866c1b5bde933ac8767
Niedziolka Michal <niedziolek@hotmail.com>
2022-05-03 21:43:43 +0200

    Update Date.setTime()

--- live-examples/js-examples/date/date-settime.js
+++ live-examples/js-examples/date/date-settime.js
@@ -1,10 +1,13 @@
-const event1 = new Date('July 1, 1999');
-const event2 = new Date();
-event2.setTime(event1.getTime());
+const launchDate = new Date('July 1, 1999, 12:00:00');
+const dateInSpace = new Date();
+dateInSpace.setTime(launchDate.getTime());
 
-console.log(event1);
-// expected output: Thu Jul 01 1999 00:00:00 GMT+0200 (CEST)
+console.log(dateInSpace);
+// expected output: Thu Jul 01 1999 12:00:00 GMT+0200 (CEST)
 
-console.log(event2);
-// expected output: Thu Jul 01 1999 00:00:00 GMT+0200 (CEST)
+const fiveMinutesInMillis = 5 * 60 * 1000;
+dateInSpace.setTime(dateInSpace.getTime() + fiveMinutesInMillis);
+
+console.log(dateInSpace);
+// expected output: Thu Jul 01 1999 12:05:00 GMT+0200 (CEST)
 // (note: your timezone may vary)
