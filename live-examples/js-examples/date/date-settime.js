ad6ef19e9ab79e7eff35750420658bd926b2c3ac
Niedziolka Michal <niedziolek@hotmail.com>
2022-05-17 16:07:03 +0200

    Rename variable

--- live-examples/js-examples/date/date-settime.js
+++ live-examples/js-examples/date/date-settime.js
@@ -1,13 +1,13 @@
 const launchDate = new Date('July 1, 1999, 12:00:00');
-const dateInSpace = new Date();
-dateInSpace.setTime(launchDate.getTime());
+const futureDate = new Date();
+futureDate.setTime(launchDate.getTime());
 
-console.log(dateInSpace);
+console.log(futureDate);
 // expected output: Thu Jul 01 1999 12:00:00 GMT+0200 (CEST)
 
 const fiveMinutesInMillis = 5 * 60 * 1000;
-dateInSpace.setTime(dateInSpace.getTime() + fiveMinutesInMillis);
+futureDate.setTime(futureDate.getTime() + fiveMinutesInMillis);
 
-console.log(dateInSpace);
+console.log(futureDate);
 // expected output: Thu Jul 01 1999 12:05:00 GMT+0200 (CEST)
 // (note: your timezone may vary)
