1e793b88992a7f1436b17de385ff682f7ef69674
Bodo Graumann <mail@bodograumann.de>
2022-06-08 07:23:19 +0200

    Make depth parameter more apparent in .flat()

--- live-examples/js-examples/array/array-flat.js
+++ live-examples/js-examples/array/array-flat.js
@@ -3,7 +3,12 @@ const arr1 = [0, 1, 2, [3, 4]];
 console.log(arr1.flat());
 // Expected output: Array [0, 1, 2, 3, 4]
 
-const arr2 = [0, 1, 2, [[3, 4]]];
+const arr2 = [0, 1, [2, [3, [4, 5]]]];
 
+// flatten with default depth 1
+console.log(arr2.flat());
+// expected output: Array [0, 1, 2, Array [3, Array [4, 5]]]
+
+// flatten with depth 2 (same as calling .flat() two times)
 console.log(arr2.flat(2));
-// Expected output: Array [0, 1, 2, Array [3, 4]]
+// expected output: Array [0, 1, 2, 3, Array [4, 5]]
