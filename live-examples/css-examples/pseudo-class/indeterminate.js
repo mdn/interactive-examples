eee1f0662ce85023ca0192378b33256f7b354a40
Niedziolka Michal <niedziolek@hotmail.com>
2022-06-18 13:55:36 +0200

    autofill, checked, default, indeterminate

---
+++ live-examples/css-examples/pseudo-class/indeterminate.js
@@ -0,0 +1,15 @@
+var newsletter = document.getElementById('newsletter');
+var premium = document.getElementById('premium');
+
+premium.indeterminate = true;
+
+newsletter.addEventListener('change', function() {
+  if (this.checked) {
+    premium.disabled = false;
+    premium.indeterminate = false;
+  } else {
+    premium.checked = false;
+    premium.disabled = true;
+    premium.indeterminate = true;
+  }
+});
