caec2fb247d4a9be41bdde71148877d62d8c17d2
Phosra <phosra@tutanota.com>
2022-07-28 07:27:42 -0700

    update select.js

--- live-examples/wat-examples/statements/select.js
+++ live-examples/wat-examples/statements/select.js
@@ -6,7 +6,7 @@ await WebAssembly.instantiateStreaming(
 
   console.log(select_10_or_20());
 
-  // if the second parameter is zero, returns the first paramater (which may be any arbitrary JS value)
+  // if the second parameter is zero, returns the first paramater (which may be an arbitrary JS value)
   const map = new Map();
   console.log(select_value_if_zero(map, 0)); // logs Map {}
   console.log(select_value_if_zero(map, -1)); // logs null
