0f47cc65138487b571efc3e93709970b5ff71e9f
Phosra <phosra@tutanota.com>
2022-07-27 18:38:40 -0700

    Update JS `return` usage

--- live-examples/wat-examples/statements/return.js
+++ live-examples/wat-examples/statements/return.js
@@ -2,6 +2,7 @@ var url = "{%wasm-url%}";
 await WebAssembly.instantiateStreaming(
   fetch(url)
 ).then(result => {
-  let get_90 = result.instance.exports.get_90;
+  const { get_90, get_second_value } = result.instance.exports;
   console.log(get_90());
+  console.log(get_second_value(7, -5));
 });
