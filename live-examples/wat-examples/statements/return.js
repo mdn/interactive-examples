bbb447d23b067fb6adccb9c2e5697e6c6e2e43ff
Phosra <phosra@tutanota.com>
2022-07-28 22:20:21 -0700

    return.js - address review

--- live-examples/wat-examples/statements/return.js
+++ live-examples/wat-examples/statements/return.js
@@ -2,7 +2,6 @@ var url = "{%wasm-url%}";
 await WebAssembly.instantiateStreaming(
   fetch(url)
 ).then(result => {
-  const { get_90, get_second_value } = result.instance.exports;
+  const { get_90 } = result.instance.exports;
   console.log(get_90());
-  console.log(get_second_value(7, -5));
 });
