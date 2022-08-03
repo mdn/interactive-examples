e6d9dfb31ec73ba0df9dbdf529f448f5e7fac337
Phosra <phosra@tutanota.com>
2022-08-03 08:50:48 -0700

    WebAssembly | Control flow - update `return` and `select` (#2238)
    * Update JS `return` usage
    
    * Update Wasm `return`
    
    Includes a discarded value on the stack
    
    * Add select.js to meta
    
    * Update select to allow usage by JS, & add `select t`
    
    * Fix function name
    
    * Create select.js
    
    * Clarify JS comment
    
    * update select.js
    
    * return.wat - address review
    
    * return.js - address review
    
    * Update select.js
    
    * Update select.wat

--- live-examples/wat-examples/statements/return.js
+++ live-examples/wat-examples/statements/return.js
@@ -2,6 +2,6 @@ var url = "{%wasm-url%}";
 await WebAssembly.instantiateStreaming(
   fetch(url)
 ).then(result => {
-  let get_90 = result.instance.exports.get_90;
+  const { get_90 } = result.instance.exports;
   console.log(get_90());
 });
