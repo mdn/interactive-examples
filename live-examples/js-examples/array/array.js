f0bcf1881e196a30f85bbcfd96bf202599477fd7
Hamir Mahal <hamirmahal@gmail.com>
2022-08-18 21:13:38 -0700

    feat: showcase basic array functionality

---
+++ live-examples/js-examples/array/array.js
@@ -0,0 +1,12 @@
+const names = [];
+
+names.push('Aisha');
+
+names.push('Bem');
+
+console.log(names); // [ 'Aisha', 'Bem' ]
+
+const top = names.pop();
+
+console.log(top);   // 'Bem'
+console.log(names); // [ 'Aisha' ]
