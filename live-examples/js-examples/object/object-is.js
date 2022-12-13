49d2a292e512d21558071601e87613b9d0bfc832
Niedziolka Michal <niedziolek@hotmail.com>
2022-12-13 15:52:22 +0100

    Add object compare

--- live-examples/js-examples/object/object-is.js
+++ live-examples/js-examples/object/object-is.js
@@ -4,8 +4,9 @@ console.log(Object.is('mdn', 'mdn'));
 console.log(Object.is(NaN, NaN));
 // expected output: true
 
-console.log(Object.is('1', true));
+console.log(Object.is(-0, 0));
 // expected output: false
 
-console.log(Object.is(-0, 0));
+let obj = {};
+console.log(Object.is(obj, {}));
 // expected output: false
