9e3d7b25249eb755e1ad969de432ab62408a2cbb
yurj26 <1816387074@qq.com>
2022-12-14 21:44:27 +0800

    Update interactive examples for comparison operators

--- live-examples/js-examples/expressions/expressions-strict-equality.js
+++ live-examples/js-examples/expressions/expressions-strict-equality.js
@@ -1,11 +1,11 @@
-console.log(1 === 1);
-// expected output: true
-
-console.log('hello' === 'hello');
-// expected output: true
-
-console.log('1' ===  1);
+console.log(1 !== 1);
 // expected output: false
 
-console.log(0 === false);
+console.log('hello' !== 'hello');
 // expected output: false
+
+console.log('1' !== 1);
+// expected output: true
+
+console.log(0 !== false);
+// expected output: true
