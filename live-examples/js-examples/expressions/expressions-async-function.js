fde520e24d485fa1c2afbfecad98eb73374d38dc
Niedziolka Michal <niedziolek@hotmail.com>
2022-12-16 18:10:57 +0100

    Lint fixes

--- live-examples/js-examples/expressions/expressions-async-function.js
+++ live-examples/js-examples/expressions/expressions-async-function.js
@@ -1,5 +1,6 @@
 function fetchUserId(userName) {
   return new Promise(resolve => {
+    console.log(`getting user id for ${userName}`);
     setTimeout(() => {
       // returning fixed id for now
       resolve(512);
@@ -7,12 +8,11 @@ function fetchUserId(userName) {
   });
 }
 
-const getUserId = async function (userName) {
-  console.log('getting user id');
+const getUserId = async function(userName) {
   return await fetchUserId(userName);
 };
 
 getUserId('admin').then((v) => {
-  console.log(v)
+  console.log(v);
   // expected output: 512
 });
