b3ff1eba5d30640fb1051e18514b8ea5a887d650
Niedziolka Michal <niedziolek@hotmail.com>
2022-12-21 15:40:50 +0100

    Add missing "Array" and fix include missing spaces

--- live-examples/js-examples/array/array-flat.js
+++ live-examples/js-examples/array/array-flat.js
@@ -6,4 +6,4 @@ console.log(arr1.flat());
 const arr2 = [0, 1, 2, [[[3, 4]]]];
 
 console.log(arr2.flat(2));
-// expected output: Array [0, 1, 2, [3, 4]]
+// expected output: Array [0, 1, 2, Array [3, 4]]
