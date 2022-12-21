d6c11775384885671af128177cfac4e90061b725
dawei-wang <dawei-wang@users.noreply.github.com>
2022-12-21 12:23:14 -0800

    Clarify error message in new.target (#2377)
    * Clarify error message 
    
    Fix mdn/content##23112
    
    * Change to constructor name

--- live-examples/js-examples/expressions/expressions-newtarget.js
+++ live-examples/js-examples/expressions/expressions-newtarget.js
@@ -1,10 +1,10 @@
 function Foo() {
-  if (!new.target) { throw 'Foo() must be called with new'; }
+  if (!new.target) { throw 'TypeError: calling Foo constructor without new is invalid'; }
 }
 
 try {
   Foo();
 } catch (e) {
   console.log(e);
-  // expected output: "Foo() must be called with new"
+  // expected output: "TypeError: calling Foo constructor without new is invalid"
 }
