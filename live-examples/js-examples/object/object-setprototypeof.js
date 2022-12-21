ed206d8f97c1188cb5404c6b77382b5562432b79
Michał Niedziółka <niedziolek@hotmail.com>
2022-12-21 21:31:35 +0100

    Add JS Object.setPrototypeOf (#2334)
    * Object.setPrototypeOf
    
    * Rewrite example
    
    * Single quote

---
+++ live-examples/js-examples/object/object-setprototypeof.js
@@ -0,0 +1,10 @@
+const obj = {};
+const parent = { foo: 'bar' };
+
+console.log(obj.foo);
+// expected output: undefined
+
+Object.setPrototypeOf(obj, parent);
+
+console.log(obj.foo);
+// expected output: "bar"
