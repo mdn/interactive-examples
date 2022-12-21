02d82c6f7dd8800ebead3969cf3ca2354053cc34
Michał Niedziółka <niedziolek@hotmail.com>
2022-12-21 21:32:12 +0100

    Add JS Object.is() (#2324)
    * Object.is()
    
    * fix quotes
    
    * Add object compare
    
    * Change string & number compare
    
    * const instead of let

---
+++ live-examples/js-examples/object/object-is.js
@@ -0,0 +1,12 @@
+console.log(Object.is('1', 1));
+// expected output: false
+
+console.log(Object.is(NaN, NaN));
+// expected output: true
+
+console.log(Object.is(-0, 0));
+// expected output: false
+
+const obj = {};
+console.log(Object.is(obj, {}));
+// expected output: false
