292154eb47c0cd1d2031de9f3f1fcc090bd22d69
Michał Niedziółka <niedziolek@hotmail.com>
2022-12-21 21:41:26 +0100

    Remove parenthesis
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- live-examples/js-examples/typedarray/typedarray-reduceright.js
+++ live-examples/js-examples/typedarray/typedarray-reduceright.js
@@ -1,7 +1,7 @@
 const uint8 = new Uint8Array([10, 20, 30]);
 
 const result = uint8.reduceRight(
-  (accumulator, currentValue) => (`${accumulator}, ${currentValue}`)
+  (accumulator, currentValue) => `${accumulator}, ${currentValue}`,
 );
 
 console.log(result);
