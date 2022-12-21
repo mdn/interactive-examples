51d6b14eb335ab6d3e5b678023ac1a40641f8c8d
Michał Niedziółka <niedziolek@hotmail.com>
2022-12-21 21:41:11 +0100

    Add parenthesis
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- live-examples/js-examples/typedarray/typedarray-foreach.js
+++ live-examples/js-examples/typedarray/typedarray-foreach.js
@@ -1,6 +1,6 @@
 const uint8 = new Uint8Array([10, 20, 30]);
 
-uint8.forEach(element => console.log(element));
+uint8.forEach((element) => console.log(element));
 
 // expected output: 10
 // expected output: 20
