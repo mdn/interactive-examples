a6f10e9fcb7a2a07748d2f8f37341802fdb6ded4
Hamir Mahal <hamirmahal@gmail.com>
2022-12-25 12:10:45 -0800

    docs: document `.length` property on arrays

--- live-examples/js-examples/array/array.js
+++ live-examples/js-examples/array/array.js
@@ -9,11 +9,13 @@ console.log(names); // [ 'Aisha', 'Bem' ]
 console.log(names[-1]); // undefined
 console.log(names[0]); // "Aisha"
 console.log(names[1]); // "Bem"
+console.log(names.length); // 2
 
 const top = names.pop();
 
 console.log(top);   // 'Bem'
 console.log(names); // [ 'Aisha' ]
+console.log(names.length); // 1
 
 const array2 = [123, "abc", { objectWithAge: 67 }, null];
 
