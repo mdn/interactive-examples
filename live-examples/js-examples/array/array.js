3d823b46c1c710ee2853a1cdea4701e3e06cf05c
Hamir Mahal <hamirmahal@gmail.com>
2022-12-25 12:04:07 -0800

    feat: demo 0-index and `[]` retrieval on arrays

--- live-examples/js-examples/array/array.js
+++ live-examples/js-examples/array/array.js
@@ -6,6 +6,10 @@ names.push('Bem');
 
 console.log(names); // [ 'Aisha', 'Bem' ]
 
+console.log(names[-1]); // undefined
+console.log(names[0]); // "Aisha"
+console.log(names[1]); // "Bem"
+
 const top = names.pop();
 
 console.log(top);   // 'Bem'
