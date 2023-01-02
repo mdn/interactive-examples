e581f799a1f2818ccf2066e2d8ee1b06c298336c
Hamir Mahal <hamirmahal@gmail.com>
2023-01-01 18:17:30 -0800

    style: make code compact by deleting empty lines

--- live-examples/js-examples/array/array.js
+++ live-examples/js-examples/array/array.js
@@ -1,23 +1,17 @@
 const names = [];
-
 names.push('Aisha');
-
 names.push('Bem');
-
 console.log(names); // [ 'Aisha', 'Bem' ]
-
 console.log(names[-1]); // undefined
 console.log(names[0]); // "Aisha"
 console.log(names[1]); // "Bem"
 console.log(names.length); // 2
 
 const top = names.pop();
-
 console.log(top); // 'Bem'
 console.log(names); // [ 'Aisha' ]
 console.log(names.length); // 1
 
 const array2 = [123, 'abc', { objectWithAge: 67 }, null];
-
 const nestedArrays = [ array2, [], [ [1, 2, 3], [4, 5, 6] ] ];
 console.log(nestedArrays.length); // 3
