4ebc2a37b098df31241054202633c9c69f9e8172
Gabe <66077254+MrBrain295@users.noreply.github.com>
2023-02-09 15:48:30 -0600

    Update bigint64.js

--- live-examples/js-examples/bigint/bigint64.js
+++ live-examples/js-examples/bigint/bigint64.js
@@ -7,10 +7,16 @@ bigint64[3] = 2156704815705747n;
 bigint64[4] = 3337090060496542n;
 
 bigint64[0] = 6118793953620967n;
-console.log(bigint64); // Expected Output: BigInt64Array [6118793953620967n, 1881938909131133n, 1898875537769492n, 2156704815705747n, 3337090060496542n]
-console.log(bigint64[2]); // Expected Output: 1898875537769492n
-console.log(`Array length:`, bigint64.length); // Expected Output: Array length: 5
-console.log(`Array byte length:`, bigint64.byteLength); // Expected Output: Array byte length: 40
-console.log(`Array byte offset:`, bigint64.byteOffset); // Expected Output: Array byte offset: 0
+console.log(bigint64);
+// Expected Output: BigInt64Array [6118793953620967n, 1881938909131133n, 1898875537769492n, 2156704815705747n, 3337090060496542n]
+console.log(bigint64[2]);
+// Expected Output: 1898875537769492n
+console.log(`Array length:`, bigint64.length);
+// Expected Output: Array length: 5
+console.log(`Array byte length:`, bigint64.byteLength);
+// Expected Output: Array byte length: 40
+console.log(`Array byte offset:`, bigint64.byteOffset);
+// Expected Output: Array byte offset: 0
 bigint64.set([100n, 200n], 1);
-console.log(bigint64); // Expected Output: BigInt64Array [6118793953620967n, 100n, 200n, 2156704815705747n, 3337090060496542n]
+console.log(bigint64);
+// Expected Output: BigInt64Array [6118793953620967n, 100n, 200n, 2156704815705747n, 3337090060496542n]
