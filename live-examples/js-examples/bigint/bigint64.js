40d9b5f89c9de2b59b50d66e982b23c56e90b7e2
Gabe <66077254+MrBrain295@users.noreply.github.com>
2023-02-10 10:11:57 -0600

    Update bigint64.js

--- live-examples/js-examples/bigint/bigint64.js
+++ live-examples/js-examples/bigint/bigint64.js
@@ -11,11 +11,11 @@ console.log(bigint64);
 // Expected Output: BigInt64Array [6118793953620967n, 1881938909131133n, 1898875537769492n, 2156704815705747n, 3337090060496542n]
 console.log(bigint64[2]);
 // Expected Output: 1898875537769492n
-console.log(`Array length:`, bigint64.length);
+console.log("Array length:", bigint64.length);
 // Expected Output: Array length: 5
-console.log(`Array byte length:`, bigint64.byteLength);
+console.log("Array byte length:", bigint64.byteLength);
 // Expected Output: Array byte length: 40
-console.log(`Array byte offset:`, bigint64.byteOffset);
+console.log("Array byte offset:", bigint64.byteOffset);
 // Expected Output: Array byte offset: 0
 bigint64.set([100n, 200n], 1);
 console.log(bigint64);
