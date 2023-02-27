8fbdc517a7dd6a8954d0657080fde01c5cec1912
jdavidleon <86429650+jdavidleon@users.noreply.github.com>
2023-02-27 12:35:44 -0500

    Semantic exmple

--- live-examples/js-examples/object/object-keys.js
+++ live-examples/js-examples/object/object-keys.js
@@ -1,8 +1,10 @@
-const object1 = {
-  a: 'somestring',
-  b: 42,
-  c: false
+const people1 = {
+  name: 'michael',
+  phone: 1111111111,
+  email: 'example@gmail.com',
+  work: false
+  
 };
 
 console.log(Object.keys(object1));
-// Expected output: Array ["a", "b", "c"]
+// Expected output: Array(4) [ "name", "phone", "email", "works" ]
