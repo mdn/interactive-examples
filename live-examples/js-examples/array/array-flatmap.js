3924b46020e9faec6651a2e2f6567a0c45a8e3be
Joshua Chen <sidachen2003@gmail.com>
2023-03-28 01:54:04 -0400

    Update live-examples/js-examples/array/array-flatmap.js

--- live-examples/js-examples/array/array-flatmap.js
+++ live-examples/js-examples/array/array-flatmap.js
@@ -1,10 +1,6 @@
 const arr1 = [1, 2, 1];
 
-const result = arr1.flatMap(num => (
-  num === 2
-    ? [2, 2]
-    : 1
-));
+const result = arr1.flatMap((num) => (num === 2 ? [2, 2] : 1));
 
 console.log(result);
 // Expected output: Array [1, 2, 2, 1]
