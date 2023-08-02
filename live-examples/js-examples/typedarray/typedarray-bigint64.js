28268a2409f5243cd3af97925752de019e81aa33
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-08-02 08:10:53 -0700

    Add missing newlines

--- live-examples/js-examples/typedarray/typedarray-bigint64.js
+++ live-examples/js-examples/typedarray/typedarray-bigint64.js
@@ -7,14 +7,19 @@ bigint64[2] = 1898875537769492n;
 bigint64[0] = 6118793953620967n;
 console.log(bigint64);
 // Expected Output: BigInt64Array [6118793953620967n, 1881938909131133n, 1898875537769492n]
+
 console.log(bigint64[2]);
 // Expected Output: 1898875537769492n
+
 console.log('Array length:', bigint64.length);
 // Expected Output: Array length: 3
+
 console.log('Array byte length:', bigint64.byteLength);
 // Expected Output: Array byte length: 24
+
 console.log('Array byte offset:', bigint64.byteOffset);
 // Expected Output: Array byte offset: 0
+
 bigint64.set([100n, 200n], 1);
 console.log(bigint64);
 // Expected Output: BigInt64Array [6118793953620967n, 100n, 200n]
