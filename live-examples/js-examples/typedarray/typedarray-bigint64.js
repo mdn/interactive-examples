09ac6503f5d0c1dc626a46c3e3f21c61983ccf56
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-08-02 08:09:55 -0700

    Shorten array

--- live-examples/js-examples/typedarray/typedarray-bigint64.js
+++ live-examples/js-examples/typedarray/typedarray-bigint64.js
@@ -1,22 +1,20 @@
-const buffer = new ArrayBuffer(40);
+const buffer = new ArrayBuffer(24);
 const bigint64 = new BigInt64Array(buffer);
 bigint64[0] = 5886014448488689n;
 bigint64[1] = 1881938909131133n;
 bigint64[2] = 1898875537769492n;
-bigint64[3] = 2156704815705747n;
-bigint64[4] = 3337090060496542n;
 
 bigint64[0] = 6118793953620967n;
 console.log(bigint64);
-// Expected Output: BigInt64Array [6118793953620967n, 1881938909131133n, 1898875537769492n, 2156704815705747n, 3337090060496542n]
+// Expected Output: BigInt64Array [6118793953620967n, 1881938909131133n, 1898875537769492n]
 console.log(bigint64[2]);
 // Expected Output: 1898875537769492n
-console.log("Array length:", bigint64.length);
-// Expected Output: Array length: 5
-console.log("Array byte length:", bigint64.byteLength);
-// Expected Output: Array byte length: 40
-console.log("Array byte offset:", bigint64.byteOffset);
+console.log('Array length:', bigint64.length);
+// Expected Output: Array length: 3
+console.log('Array byte length:', bigint64.byteLength);
+// Expected Output: Array byte length: 24
+console.log('Array byte offset:', bigint64.byteOffset);
 // Expected Output: Array byte offset: 0
 bigint64.set([100n, 200n], 1);
 console.log(bigint64);
-// Expected Output: BigInt64Array [6118793953620967n, 100n, 200n, 2156704815705747n, 3337090060496542n]
+// Expected Output: BigInt64Array [6118793953620967n, 100n, 200n]
