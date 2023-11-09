411b5a70132d69cf3ff34afc4274e58501e330d2
Brian Thomas Smith <brian@smith.berlin>
2023-11-09 16:51:36 +0100

    docs(CSS): Improvements following reviewer feedback

--- live-examples/webapi-examples/url/url-constructor.js
+++ live-examples/webapi-examples/url/url-constructor.js
@@ -1,7 +1,7 @@
-const url1 = new URL('https://example.com/wiki/Mozilla#Software');
+const url1 = new URL('https://en.wikipedia.org/wiki/Mozilla#Software');
 console.log(url1.toString());
-// expected output: "https://example.com/wiki/Mozilla#Software"
+// expected output: "https://en.wikipedia.org/wiki/Mozilla#Software"
 
-const url2 = new URL('wiki/Mozilla#Software', 'https://example.com');
+const url2 = new URL('wiki/Mozilla#Software', 'https://en.wikipedia.org');
 console.log(url2.toString());
-// expected output: "https://example.com/wiki/Mozilla#Software"
+// expected output: "https://en.wikipedia.org/wiki/Mozilla#Software"
