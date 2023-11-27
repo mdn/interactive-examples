d436ed28c3fab62193198abd8f39560aa3cc716e
Himanshu Chauhan <codebreaker8055@gmail.com>
2023-11-27 09:39:28 +0000

    Again-update-example

--- live-examples/js-examples/regexp/regexp-groups-ranges.js
+++ live-examples/js-examples/regexp/regexp-groups-ranges.js
@@ -6,8 +6,8 @@ console.log(`Width: ${match[1]} / Height: ${match[2]}.`);
 // Expected output: "Width: 1440 / Height: 900."
 
 // backreferences
-const aliceExcerpt =
-  "This is is some text text with double double words some where I I am not not sure.";
-const regexpWithoutE = /\b(\w+)\s\1\b/g;
-console.log(aliceExcerpt.match(regexpWithoutE));
-// Expected output: Array ['is is', 'text text', 'double double', 'I I', 'not not']
+const quote = `Single quote "'" and double quote '"'`;
+const regexpQuotes = /(['"]).*?\1/g;
+for (const match of quote.matchAll(regexpQuotes)) {
+  console.log(match[0]);
+}
