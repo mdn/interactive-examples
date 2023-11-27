0b8d25b644bc8441469d1e5446ecbe1c3eaf04f6
Himanshu Chauhan <codebreaker8055@gmail.com>
2023-11-27 09:13:14 +0000

    Run Prettier

--- live-examples/js-examples/regexp/regexp-groups-ranges.js
+++ live-examples/js-examples/regexp/regexp-groups-ranges.js
@@ -1,12 +1,13 @@
 // Groups
-const imageDescription = 'This image has a resolution of 1440×900 pixels.';
+const imageDescription = "This image has a resolution of 1440×900 pixels.";
 const regexpSize = /([0-9]+)×([0-9]+)/;
 const match = imageDescription.match(regexpSize);
 console.log(`Width: ${match[1]} / Height: ${match[2]}.`);
 // Expected output: "Width: 1440 / Height: 900."
 
 // backreferences
-const aliceExcerpt = 'This is is some text text with double double words some where I I am not not sure why why I am typing ok? rainbow rainbow unicorn unicorn.';
+const aliceExcerpt =
+  "This is is some text text with double double words some where I I am not not sure why why I am typing ok? rainbow rainbow unicorn unicorn.";
 const regexpWithoutE = /\b(\w+)\s\1\b/g;
 console.log(aliceExcerpt.match(regexpWithoutE));
 // Expected output: Array ['is is', 'text text', 'double double', 'I I', 'not not', 'why why', 'rainbow rainbow', 'unicorn unicorn']
