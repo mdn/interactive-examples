426fd3266a80808dc80192054cf5e09ea965d308
Himanshu Chauhan <codebreaker8055@gmail.com>
2023-11-27 10:07:23 +0000

    New-line-add-again

--- live-examples/js-examples/regexp/regexp-groups-ranges.js
+++ live-examples/js-examples/regexp/regexp-groups-ranges.js
@@ -5,9 +5,11 @@ const match = imageDescription.match(regexpSize);
 console.log(`Width: ${match[1]} / Height: ${match[2]}.`);
 // Expected output: "Width: 1440 / Height: 900."
 
-// backreferences
+// Backreferences
 const quote = `Single quote "'" and double quote '"'`;
 const regexpQuotes = /(['"]).*?\1/g;
 for (const match of quote.matchAll(regexpQuotes)) {
   console.log(match[0]);
 }
+// Expected output: '"'"' '"'"'
+
