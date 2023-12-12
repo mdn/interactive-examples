a5d0158962a94d764e8a72a6fda19b368a1c68f8
Himanshu Chauhan <codebreaker8055@gmail.com>
2023-12-12 13:53:54 +0530

    [Fix]groups-and-backreferences-#2664 (#2671)
    * [Fix]groups-and-backreferences-#2664
    
    * Update-example
    
    * Add-new-line-end-of-the-code
    
    * Run Prettier
    
    * Update-code
    
    * Again-update-example
    
    * New-line-add-again
    
    * Fix formatting for Prettier
    
    * Change-other-example
    
    * Update regexp-groups-ranges.js
    
    * Update file name
    
    ---------
    
    Co-authored-by: Vadim Makeev <hi@pepelsbey.dev>
    Co-authored-by: Joshua Chen <sidachen2003@gmail.com>

--- live-examples/js-examples/regexp/regexp-groups-backreferences.js
+++ live-examples/js-examples/regexp/regexp-groups-backreferences.js
@@ -1,10 +1,12 @@
-const aliceExcerpt = 'The Caterpillar and Alice looked at each other';
-const regexpWithoutE = /\b[a-df-z]+\b/gi;
-console.log(aliceExcerpt.match(regexpWithoutE));
-// Expected output: Array ["and", "at"]
-
+// Groups
 const imageDescription = 'This image has a resolution of 1440×900 pixels.';
 const regexpSize = /([0-9]+)×([0-9]+)/;
 const match = imageDescription.match(regexpSize);
 console.log(`Width: ${match[1]} / Height: ${match[2]}.`);
 // Expected output: "Width: 1440 / Height: 900."
+
+// Backreferences
+const findDuplicates = 'foo foo bar';
+const regex = /\b(\w+)\s+\1\b/g;
+console.log(findDuplicates.match(regex));
+// Expected output: Array ["foo foo"]
