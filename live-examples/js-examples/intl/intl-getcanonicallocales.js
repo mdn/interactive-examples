6a8ee159f005786debcf785d6673d0973719e327
btea <2356281422@qq.com>
2024-02-23 22:42:44 +0800

    Update live-examples/js-examples/intl/intl-getcanonicallocales.js
    Co-authored-by: Brian Thomas Smith <brian@smith.berlin>

--- live-examples/js-examples/intl/intl-getcanonicallocales.js
+++ live-examples/js-examples/intl/intl-getcanonicallocales.js
@@ -8,5 +8,7 @@ try {
   Intl.getCanonicalLocales('EN_US');
 } catch (err) {
   console.log(err.toString());
-  // Expected output: RangeError: Incorrect locale information provided
+  // Expected output: 
+  //   (Firefox/Safari) RangeError: invalid language tag: "EN_US"
+  //   (Chrome) RangeError: Incorrect locale information provided
 }
