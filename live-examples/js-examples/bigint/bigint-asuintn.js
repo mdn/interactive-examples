39021dc9c6c61c0d1264c547b139f817d7046dad
lionel-rowe <lionel.rowe@gmail.com>
2024-08-31 11:16:38 +0800

    Demonstrate wrapping with asIntN/asUintN Bigint static methods

--- live-examples/js-examples/bigint/bigint-asuintn.js
+++ live-examples/js-examples/bigint/bigint-asuintn.js
@@ -1,13 +1,12 @@
-const max = 2n ** 64n - 1n;
+const U64_CEIL = 2n ** 64n;
 
-function check64bit(number) {
-  number > max
-    ? console.log("Number doesn't fit in unsigned 64-bit integer!")
-    : console.log(BigInt.asUintN(64, number));
-}
-
-check64bit(2n ** 64n);
-// Expected output: "Number doesn't fit in unsigned 64-bit integer!"
-
-check64bit(2n ** 32n);
-// Expected output: 4294967296n
+console.log(BigInt.asUintN(64, U64_CEIL - 1n));
+// 18446744073709551615n (2n ** 64n - 1n, the maximum non-wrapping value)
+console.log(BigInt.asUintN(64, U64_CEIL));
+// 0n (wraps to zero)
+console.log(BigInt.asUintN(64, U64_CEIL + 1n));
+// 1n
+console.log(BigInt.asUintN(64, U64_CEIL * 2n));
+// 0n (wraps on multiples)
+console.log(BigInt.asUintN(64, U64_CEIL * -42n));
+// 0n (also wraps on negative multiples)
